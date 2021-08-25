30f70afe4e40c5e9cb7abcfe119e7a819d6d4c81
nathanjukes <njukes1@gmail.com>
2021-08-25 14:36:58 +0100

    Finished song cards

--- src/components/ActivityTable.vue
+++ src/components/ActivityTable.vue
@@ -4,49 +4,60 @@
         <article class="overflow-hidden rounded-lg shadow-lg">
           <div class="flex flex-row">
               <div>
-                <a href="#">
+                <a :href="a.track_url">
                 <img alt="Placeholder" class="object-cover h-48 w-48" :src="a.track.imageUrl">
                 </a>
               </div>
               <div class="flex-1">
-                <div class="flex flex-col">
-                    <div>
-                      <header class="flex items-center justify-between leading-tight p-2 md:p-4">
+                <div class="flex flex-col h-48 w-64">
+                  <div>
+                  <header class="flex items-center justify-between leading-tight p-2 md:p-4">
                           <h1 class="text-md">
-                            <a class="no-underline hover:underline text-black line-clamp-2 overflow-ellipsis" href="#">
+                            <a class="no-underline hover:underline text-black line-clamp-2 overflow-ellipsis" :href="a.track_url">
                             {{ a.track.name }} 
                             </a>
-                            <br>{{ a.track.artist.name }}
+                            <br>
+                            <a class="no-underline hover:underline text-black line-clamp-2 overflow-ellipsis" :href="a.track.artist_url">
+                              {{ a.track.artist.name }}
+                            </a>
                           </h1>
-                          <p class="text-grey-darker text-sm">
-                            {{ moment(a.time).format("DD/MM/YYYY HH:mm") }}
-                          </p>
                       </header>
-                    </div>
-                    <div>
-                      <footer class="flex items-center justify-between leading-none p-2 md:p-4">
-                          <a class="flex items-center no-underline hover:underline text-black" href="#">
-                            <img class="inline object-cover w-12 h-12 mr-2 rounded-full" :src="a.user.imageUrl">
-                            <p class="text-sm">
-                                {{ a.user.name }}
-                            </p>
-                          </a>
-                          <a class="no-underline text-grey-darker hover:text-red-dark" href="#">
-                          <span class="hidden">Link</span>
-                          <i class="fa fa-heart"></i>
-                          </a>
-                      </footer>
-                    </div>
+                      </div>
+                      <div class="align-bottom">
+                         <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+            <a class="flex items-center no-underline hover:underline text-black" :href="a.user.user_url">
+              <img class="inline object-cover w-12 h-12 mr-2 rounded-full" :src="a.user.imageUrl">
+              <p class="text-sm">
+                  {{ a.user.name }}
+              </p>
+            </a>
+          </footer>
+                      </div>
+                </div> 
+              </div>
+                <div class="flex flex-col h-48">
+                  <div class="h-1/2">
+                                 <header class="flex items-center justify-between leading-tight p-1 md:p-2 mt-7">
+                          <h1 class="text-md">
+                            <span class="no-underline text-black">
+                          {{ getHumanDate(a.time) }} </span>
+                          </h1>
+                      </header>
+                        </div>
+                        <div>
+            <a class="no-underline text-grey-darker hover:text-red-dark" :href="a.track_url">
+            <span class="hidden">Link</span>
+            <i class="fa fa-heart float-right mt-9 m-7"></i> </a>
+                        </div>
                 </div>
               </div>
-          </div>
         </article>
       </div> 
     </div>
 </template>
 
 <script>
-import moment from "moment";
+import moment from 'moment';
 
 export default {
     name: 'activity-table',
@@ -54,13 +65,10 @@ export default {
         activities: Array,
     },
     methods: {
-        getSpotifyUrl() {
-
-        },
-    moment: function () {
-      return moment();
-      }
-    }
+            getHumanDate : function (date) {
+                return moment(date).format('DD/MM/YYYY HH:mma');
+            }
+        }
 }
 </script>
 
--- src/components/UserSearchForm.vue
+++ src/components/UserSearchForm.vue
@@ -2,7 +2,7 @@
     <div id="user-search-form">
         <div class="p-8">
   <div class="bg-white flex items-center rounded-full shadow-xl" id="user-search-form">
-    <input class="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none" id="search" type="text" placeholder="Search" v-model="user.user_id">
+    <input class="rounded-l-full w-full py-4 px-6 text-gray-700 leading-tight focus:outline-none" id="search" type="text" placeholder="Spotify User URL" v-model="user.user_id">
     <div class="p-4">
       <button v-on:click="handleSubmit" class="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center"><i class="fas fa-search"></i></button>
       </div>
